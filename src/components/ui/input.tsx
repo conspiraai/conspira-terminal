566c4a974e0db82871a4aabce9b0ce3049065784
conspiraai <conspiraai@gmail.com>
2025-08-26 18:50:29 -0400

    Update input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,17 +1,19 @@
 import * as React from "react";
 
-export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
-  return (
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className = "", ...props }, ref) => (
     <input
+      ref={ref}
+      className={
+        "w-full rounded-md border border-white/20 bg-black/60 px-3 py-2 " +
+        "text-sm text-white placeholder:text-gray-400 focus:outline-none " +
+        "focus:ring-2 focus:ring-yellow-400/40 " +
+        className
+      }
       {...props}
-      style={{
-        padding: "8px",
-        border: "1px solid #333",
-        borderRadius: "4px",
-        background: "#000",
-        color: "#fff",
-      }}
-      placeholder={props.placeholder || "System Mutating…"}
     />
-  );
-}
+  )
+);
+Input.displayName = "Input";
